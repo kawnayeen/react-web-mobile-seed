8e9863ef13ab31ec5de156760ea908373de095e9
kamarul kawnayeen <kawnayeen.08@gmail.com>
2017-11-29 00:26:23 +0600

    using business-logic in react web app

--- business-logic/index.js
+++ business-logic/index.js
@@ -1,3 +1,3 @@
 export function sum(num1, num2){
-	return sum1 + sum2;
+	return num1 + num2;
 }
--- web-app/src/App.js
+++ web-app/src/App.js
@@ -1,19 +1,10 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { sum } from 'business-logic';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
+      <p>Sum of 3 & 4 is {sum(3,4)}</p>
     );
   }
 }
